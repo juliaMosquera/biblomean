fb56729d3e655acb1c626fcd752b897e40c52038
juliaMosquera <julia.llarisa@gmail.com>
2022-02-18 12:06:07 -0500

    actualizando biblomean

--- backend/controllers/bookController.js
+++ backend/controllers/bookController.js
@@ -1,7 +1,9 @@
 import book from "../models/book.js";
+import jwt from "jsonwebtoken";
+import moment from "moment";
 
 const registerBook = async (req, res) => {
-  if (!req.body.author || !req.body.category || !req.body.editorial || !req.body.pages)
+  if (!req.body.title || !req.body.author || !req.body.category || !req.body.pages)
     return res.status(400).send({ message: "Imcomplete data" });
 
   let schema = new book({
@@ -18,12 +20,25 @@ const registerBook = async (req, res) => {
   if (!result)
     return res.status(500).send({ message: "Failed to register book" });
 
-  res.status(200).send({ result });
+    try {  
+      return res.status(200).json({
+          token: jwt.sign({
+              _id: result._id,
+              title: result.title,
+              user: result.user,
+              iat: moment().unix()
+          },
+          process.env.SK_JWT
+          ),
+      });
+      } catch (e) {
+          return res.status(500).send({message: "Register error"});
+      }
 };
 
 const listBook = async (req, res) => {
   let books = await book.find({ title: new RegExp(req.params["title"])})
-  .populate("book")
+  .populate("user")
   .exec();
   if(books.length === 0)
   return res.status(400).send({ message: "No search results"})
--- backend/middleware/bookValidate.js
+++ backend/middleware/bookValidate.js
@@ -1,11 +1,11 @@
 import book from "../models/book.js";
 
 const existingBook = async (req, res, next) => {
-    if(!req.body.title)
+    if(!req.body.editorial)
     return res.status(400).send({ message: "Incomplete data"});
 
-    const existingTitle = await book.findOne({book: req.body.title});
-    if(existingTitle)
+    const existingEditorial = await book.findOne({editorial: req.body.editorial});
+    if(existingEditorial)
     return res.status(400).send({message: "The book already registered"})
     next();
 }
