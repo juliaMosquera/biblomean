0686dee1462ed5188266de4188fda3735aa00d17
juliaMosquera <julia.llarisa@gmail.com>
2022-03-02 17:11:52 -0500

    Agregando frontend

---
+++ frontend/src/app/app-routing.module.ts
@@ -0,0 +1,65 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ListRoleComponent } from './admin/list-role/list-role.component';
+import { ListUserComponent } from './admin/list-user/list-user.component';
+import { RegisterRoleComponent } from './admin/register-role/register-role.component';
+import { RegisterUserComponent } from './admin/register-user/register-user.component';
+import { UpdateRoleComponent } from './admin/update-role/update-role.component';
+import { UpdateUserComponent } from './admin/update-user/update-user.component';
+import { ListBookComponent } from './board/list-book/list-book.component';
+import { SaveBookComponent } from './board/save-book/save-book.component';
+import { LoginComponent } from './home/login/login.component';
+import { RegisterComponent } from './home/register/register.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: LoginComponent,
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: 'listBook',
+    component: ListBookComponent,
+  },
+  {
+    path: 'saveBook',
+    component: SaveBookComponent,
+  },
+  {
+    path: 'signUp',
+    component: RegisterComponent,
+  },
+  {
+    path: 'registerUser',
+    component: RegisterUserComponent,
+  },
+  {
+    path: 'listUser',
+    component: ListUserComponent,
+  },
+  {
+    path: 'updateUser',
+    component: UpdateUserComponent,
+  },
+  {
+    path: 'registerRole',
+    component: RegisterRoleComponent,
+  },
+  {
+    path: 'listRole',
+    component: ListRoleComponent,
+  },
+  {
+    path: 'updateRole',
+    component: UpdateRoleComponent,
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ frontend/src/app/home/register/register.component.ts
@@ -0,0 +1,74 @@
+import { Component, OnInit } from '@angular/core';
+import { UserService } from "../../services/user.service";
+import { Router } from "@angular/router";
+import { 
+  MatSnackBar, 
+  MatSnackBarHorizontalPosition, MatSnackBarVerticalPosition 
+} from "@angular/material/snack-bar";
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+ registerData: any;
+ message: string = '';
+ horizontalPosition: MatSnackBarHorizontalPosition = 'end';
+verticalPosition: MatSnackBarVerticalPosition = 'top';
+durationInSeconds: number = 2000;
+
+  constructor(
+    private _userService: UserService, 
+    private _router: Router, 
+    private _snackBar: MatSnackBar
+  ) { 
+    this.registerData = {}
+  }
+
+  registerUser(){
+    if (
+      !this.registerData.name || 
+      !this.registerData.email || 
+      !this.registerData.password ||
+      !this.registerData.address ||
+      !this.registerData.phone
+      ) {
+        this.message = 'Incomplete data'
+        this.openSnackBarError();
+    } else {
+      this._userService.registerUser(this.registerData).
+       subscribe({
+         next: (v) => {
+          localStorage.setItem('token', v.token)
+          this._router.navigate(['/saveBook']);
+          this.message = 'Succesfull user registration';
+          this.openSnackBarSuccesfull();
+         },
+         error:(e) => {
+          this.message = e.error.message;
+          this.openSnackBarError();
+         },
+      });
+    }
+  }
+  openSnackBarSuccesfull(){
+    this._snackBar.open(this.message, 'X', {
+      horizontalPosition: this.horizontalPosition,
+      verticalPosition: this.verticalPosition,
+      duration: this.durationInSeconds,
+      panelClass:['styleSnackBarSuccesfull']
+    })
+  }
+  openSnackBarError(){
+    this._snackBar.open(this.message, 'X', {
+      horizontalPosition: this.horizontalPosition,
+      verticalPosition: this.verticalPosition,
+      duration: this.durationInSeconds,
+      panelClass:['styleSnackBarError']
+    })
+  }
+
+  ngOnInit(): void {}
+
+}
